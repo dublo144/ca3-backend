b0f6366bd17e533ce2a6e8177ce8d5cbef6698d8
Mads Brandt <mads.brandt93@gmail.com>
2020-04-19 13:45:07 +0200

    trigger travis

--- src/main/java/rest/JokeResource.java
+++ src/main/java/rest/JokeResource.java
@@ -50,7 +50,6 @@ import java.io.IOException;
         }
 )
 @Path("jokes")
-// Comment to trigger travis build
 public class JokeResource {
     private final String DAD_URL = "https://icanhazdadjoke.com";
     private final String CHUCK_URL = "https://api.chucknorris.io/jokes/random";
